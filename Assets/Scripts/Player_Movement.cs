458827a16c5e600f2a7a33d3d0d7518c6aa2029a
Artyom <shumaylov2000@gmail.com>
2024-11-22 12:33:27 +0300

    Добавил движение и усорение

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -7,15 +7,16 @@ using UnityEngine;
 //Основан на коде движение из Quake 3, адаптированного под Юнити
 public class Player_Movement : MonoBehaviour
 {
+    public float Gravity_Force;
+
     public Move_Stats Ground_Stats; //Скорость игрока на земле
     public float Current_Friction; //Сила трения на земле
 
     public Move_Stats Air_Stats; //Скорость игрока в воздухе
     public Move_Stats Airstrafe_Stats; //Скорость игрока при движении в бок в воздухе
-    public float Air_Control; //Маневренность игрока в воздухе
 
 
-    public Vector3 Velocity;
+    [SerializeField] private Vector3 Velocity;
 
     CharacterController CharacterController_;
     Player_Camera Player_Camera_;
@@ -39,20 +40,50 @@ public class Player_Movement : MonoBehaviour
         Vector3 cam_forward = new Vector3(0, Camera.main.transform.forward.y, 0);
         gameObject.transform.localRotation = Player_Camera_.Rotation_Character;
 
+        Vector3 wishdir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        wishdir = transform.TransformDirection(wishdir);
+        wishdir.Normalize();
+
         if (CharacterController_.isGrounded)
         {
-            Move_Ground();
+            Move_Ground(wishdir);
         }
         else
         {
-            Move_Air();
+            Move_Air(wishdir);
+        }
+        Gravity();
+
+        CharacterController_.Move(Velocity);
+    }
+    //Влияние гравитации
+    void Gravity()
+    {
+        if (CharacterController_.isGrounded)
+        {
+            Velocity.y = -1 * Gravity_Force * Time.deltaTime;
+        }
+        else
+        {
+            Velocity.y += -1 * Gravity_Force * Time.deltaTime;
         }
     }
-    void Move_Ground()
+    //Изменяет ускорение игрока
+    void Accelerate(Vector3 dir, float target_speed, float acceleration)
     {
-        //TODO:
+        Velocity.x += dir.x * target_speed * acceleration * Time.deltaTime;
+        Velocity.z += dir.z * target_speed * acceleration * Time.deltaTime;
     }
-    void Move_Air()
+    //Движение по земле
+    void Move_Ground(Vector3 wishdir)
+    {
+        //TODO: Сила трения и снижение усорения
+        float maxSpeed = wishdir.magnitude * Ground_Stats.Max_Speed;
+        Accelerate(wishdir, maxSpeed, Ground_Stats.Acceleration);
+
+    }
+    //Движение в воздухе
+    void Move_Air(Vector3 wishdir)
     {
         //TODO:
     }

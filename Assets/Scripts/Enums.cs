4097c8b6d60ff0f377730a85cf4b3b9c5ca9da65
Artyom <shumaylov2000@gmail.com>
2024-11-26 13:49:42 +0300

    Поработал над созданием оружия

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,1 @@
+public enum Weapon_Type { Melee, Ranged };
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+//Скриптовый объект оружия
+//Файл, который хранит данные принадлежащие оружию. 
+//На его основе можно быстро создавать новые виды оружия
+
+/*
+    Характеристики оружия:
+    Имя
+    Тип (Melee/ Ranged)
+
+    
+ */
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Weapon", order = 1)]
+public class Weapon : ScriptableObject
+{
+    public string Name;
+
+    public Weapon_Type Type;
+}

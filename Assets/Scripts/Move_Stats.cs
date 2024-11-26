98f57e0a20c78a93a39d66abde841f11eebade31
Artyom <shumaylov2000@gmail.com>
2024-11-26 13:42:01 +0300

    Перенес стрельбу и здоровья для работы над стрельбой

---
+++ Assets/Scripts/Move_Stats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Используется для хранения информации о скорости игрока в разных состояниях (на земле, воздухе и т.д.)
+[System.Serializable]
+public class Move_Stats 
+{
+    public float Max_Speed;
+    public float Acceleration;
+    public float Deceleration;
+}
